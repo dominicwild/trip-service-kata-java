35d338e94dd7d56bf64a11540bba9e6426a86093
dwild8 <dwild8@dxc.com>
2022-03-10 12:30:59 +0000

    Remove variables

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,6 +1,7 @@
 package org.craftedsw.tripservicekata.trip;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
@@ -10,16 +11,15 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<>();
 		if (!userIsLoggedIn()) {
 			throw new UserNotLoggedInException();
 		}
-
+		
 		if (user.isFriendsWith(loggedInUser())) {
-			tripList = tripsBy(user);
+			return tripsBy(user);
 		}
 		
-		return tripList;
+		return Collections.emptyList();
 	}
 
 	private boolean userIsLoggedIn() {
